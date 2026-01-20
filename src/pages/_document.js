35cc35d8c54f2d1f2c38a7c5df3565d38b9ab54c
Sabina Huseynzada <sebine.huseyn0@gmail.com>
2026-01-20 21:33:46 +0400

    document changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
+      <Head>
+        <link rel="icon" href="/images/key.webp" type="image/webp" />
+      </Head>
       <body>
         <Main />
         <NextScript />
